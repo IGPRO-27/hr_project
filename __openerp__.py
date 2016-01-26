8a4ac7ef8efa3e869094e5dd4f1065d9e4d7aa81
Sidwise <sid_wise@hotmail.com>
2016-01-26 16:18:39 +0100

    intial commit

---
+++ __openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hr_project_custom",
+
+    'summary': """Link Tasks to projects""",
+
+    'description': """
+        Link Tasks to projects:
+    """,
+
+    'author': "IGPRO",
+    'website': "http://igpro-online.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
+        # 'views/templates.xml',
+        # 'views/session_workflow.xml',
+        # 'views/openacademy.xml',
+        # 'views/partner.xml',
+        # 'views/session_board.xml',
+        # 'views/report.xml',
+
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    ],
+}
