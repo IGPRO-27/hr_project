b1d79643f272ee3a483c9445cff32c71a079af9f
Sidwise <sid_wise@hotmail.com>
2016-02-03 13:44:07 +0100

    Fix creation of works in timesheet module
    Make use of the new Api
    Remove the user_id and replace it by the context env.uid
    link the task_id related field with task_id.id

--- models/hrproject.py
+++ models/hrproject.py
@@ -4,61 +4,59 @@ from openerp import models, fields, api, exceptions
 
 class hrproject(models.TransientModel):
     _name = 'hr.sheet.wizard'
-    # _inherit = 'hr.analytic.timesheet
 
+   # user_id = fields.Many2one('res.users', 'user', required=True)
     sheet_id =  fields.Many2one("hr_timesheet_sheet.sheet", string="Sheets", required=True)
     description = fields.Char(string ="Description" ,  required=True)
-    Task_ids = fields.Many2one("project.task",string="Tache",required=True)
+    Task_ids = fields.Many2one("project.task", string="Tache", required=True)
     hour = fields.Float(string="Time Spent" , required=True)
     work_ids =  fields.Many2one("project.task.work", string="Works")
 
+    # def create(self,cr, uid, context=None, user_id)
 
-    @api.one
+    # if context is None:context ={}
+    # model_name=context.get('active_model')
+    #     emp_obj = self.pool.get('hr.employee')
+    #     emp_id = emp_obj.search(cr, uid, [('user_id', '=', user_id)])
+        # print "L'UTILISATEUR EST %s " %(emp_id.name)
+
+    @api.multi
     def save(self):
+
+       # emp_obj = self.pool.get('hr.employee')
+       # emp_id = self.env('hr.employee').search([('user_id', '=',self.user_id)])
+        print "--------------------------------------------"
+        print self.env.uid
+
         # self.work_ids.display_name=self.description
         # self.work_ids.hours=self.hour
         # self.work_ids.task_id=self.Task_ids
 
-        #call create function
-        record = self.env['project.task.work'].create({
+        # uid=self._uid
+        # print "--------------------------------------------"
+        # print uid
+        # ids=self._ids
+        # print "--------------------------------------------"
+        # print ids
+        # context=self._context
+        # print "--------------------------------------------"
+        # print context
+        # # cr=self._cr.fetchone()[0]
+
+        # # print "--------------------------------------------"
+        # # print cr
+
+        # if context is None: context = {}
+
+        # active_id  = context.get('active_id',False)
+
+        self.env['project.task.work'].create({
             'name':self.description,
             'hours':self.hour,
             'task_id':self.Task_ids.id,
+            'user_id':self.env.uid,
             })
+        return {}
 
-        #self.sheet_id.timesheet_ids |= self.work_ids.hr_analytic_timesheet_id
-        return res_id
-
-    # TypeError: cannot convert dictionary update sequence element #0 to a sequence
-
-    # work_ids = fields.Many2one('project.task.work','hr_analytic_timesheet_id', String="Works")
-
-    # _sql_constraints = [
-    #     ('work_ids_constraint',
-    #      'unique(work_ids)',
-    #      'Choose another value - it has to be unique! please check your work')
-    # ]
-
-    # def _auto_init(self, cr, context=None):
-    #     self._sql_constraints = self._sql_constraints.reverse()
-    #     super(hrproject, self)._auto_init(cr, context)
-
-# class hrinheritsproject(models.Model):
-#     _name = 'hr.analytic.timesheet.delegate'
-#     _inherits = {'hr.analytic.timesheet':'work_ids'}
-
-#     work_ids = fields.Many2one('project.task.work','hr_analytic_timesheet_id', String="Works", auto_join=True)
-
-
-
-    # @api.model
-    # def create(self, work_ids):
-    #     record = super(hrproject, self).create(work_ids)
-    #     print record
-    #     return record
-
-    # @api.one
-    # @api.constrains(work_ids)
-    # def _work_unique(self):
-    #     if self == self.work_ids:
-    #         raise exceptions.ValidationError("ID not unique")
+       #self.sheet_id.timesheet_ids |= self.work_ids.hr_analytic_timesheet_id
+       # return {}
